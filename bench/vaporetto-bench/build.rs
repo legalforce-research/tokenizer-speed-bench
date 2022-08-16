e841584d8367352df7004607510f55515ec5b86f
kampersanda <shnsk.knd@gmail.com>
2022-08-16 06:11:45 +0000

    to 0.5.1

--- bench/vaporetto-bench/build.rs
+++ bench/vaporetto-bench/build.rs
@@ -3,18 +3,16 @@
 use std::convert::TryFrom;
 use std::env;
 use std::fs::File;
-use std::io::{BufWriter, Cursor, Write};
+use std::io::{BufWriter, Cursor};
 use std::path::PathBuf;
 
-use vaporetto::{KyteaModel, Model, Predictor};
+use vaporetto::{KyteaModel, Model};
 
 fn main() {
-    let mut model_data = Cursor::new(include_bytes!("../../resources/jp-0.4.7-5.mod"));
-    let model = KyteaModel::read(&mut model_data).unwrap();
+    let model_data = Cursor::new(include_bytes!("../../resources/jp-0.4.7-5.mod"));
+    let model = KyteaModel::read(model_data).unwrap();
     let model = Model::try_from(model).unwrap();
-    let predictor = Predictor::new(model, false).unwrap();
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
     let mut buf = BufWriter::new(File::create(out.join("predictor.bin")).unwrap());
-    let model_data = predictor.serialize_to_vec().unwrap();
-    buf.write_all(&model_data).unwrap();
+    model.write(&mut buf).unwrap();
 }
