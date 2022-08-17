23c860adf582a931f5cfe7b864e13a9a673ecd60
kampersanda <shnsk.knd@gmail.com>
2022-08-17 01:48:24 +0000

    Revert "reformat"
    This reverts commit 90a780a69448653d59aa93fec3f4ad7c47bf664b.

--- bench/mecab-unidic-2_1_2-bench/main.cc
+++ bench/mecab-unidic-2_1_2-bench/main.cc
@@ -8,8 +8,8 @@
 
 using namespace MeCab;
 
-int main(int argc, const char **argv) {
-    Tagger *tagger = createTagger("-Owakati -d./resources/unidic-mecab-2.1.2_bin");
+int main(int argc, const char** argv) {
+    Tagger* tagger = createTagger("-Owakati -d./resources/unidic-mecab-2.1.2_bin");
 
     std::vector<std::string> lines;
     for (std::string line; std::getline(std::cin, line);) {
@@ -17,14 +17,13 @@ int main(int argc, const char **argv) {
     }
     std::vector<char> buf(65536);
     const std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    for (std::string &line : lines) {
+    for (std::string& line : lines) {
         std::strcpy(buf.data(), line.c_str());
-        const volatile char *r = tagger->parse(buf.data());
+        const volatile char* r = tagger->parse(buf.data());
     }
     const std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     const auto d = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
-    std::cout << "Elapsed-mecab-unidic-2_1_2: " << static_cast<double>(d) / 1000000 << " [sec]"
-              << std::endl;
+    std::cout << "Elapsed-mecab-unidic-2_1_2: " << static_cast<double>(d) / 1000000 << " [sec]" << std::endl;
 
     return 0;
 }
